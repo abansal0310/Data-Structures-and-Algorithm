7f7e5123c879e5e76ef43247b8a9a1044c537b3e
abansal0310 <akshitbansal0310@gmail.com>
2023-09-08 10:49:50 +0530

    8th September, 2023

---
+++ Sorting Algorithms/bubbleSort.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+void swap(int *a, int *b){
+    int temp=*a;
+    *a=*b;
+    *b=temp;
+}
+
+void bubbleSort(int arr[],int n){
+    for(int i=0;i<n;i++){
+        
+    }
+}
+
+int main(){
+
+}
---
+++ Stacks/infixTopostfix.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int value(char x){
+    if(x=='^')
+        return 3;
+    else if(x=='*' || x=='/')
+        return 2;
+    else if(x=='+' || x=='-')
+        return 1;
+    else
+        return -1;
+}
+
+void conversion(string s){
+    stack<char>ans;
+
+    int str = s.length();
+    string ns;
+
+    for(int i=0;i<str;i++){
+    }
+
+}
+
+int main(){
+
+}
